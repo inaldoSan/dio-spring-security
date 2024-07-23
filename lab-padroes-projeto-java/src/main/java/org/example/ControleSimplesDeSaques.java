dc9e075a58eb4e9730f7470cea193c0acc5353e7
Inaldo Gonçalves <inaldo.gsanjinez@gmail.com>
2024-07-23 03:31:54 -0300

    edit

---
+++ lab-padroes-projeto-java/src/main/java/org/example/ControleSimplesDeSaques.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double limiteDiario = scanner.nextDouble();
+
+        while (true) {
+            double saque = scanner.nextDouble();
+            if (saque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break;
+            } else if (limiteDiario >= saque) {
+                limiteDiario -= saque;
+                System.out.println("Saque realizado. Limite restante: " + limiteDiario);
+            } else {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
